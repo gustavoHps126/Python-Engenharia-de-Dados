d5af6a7311f2c5177286209dd6f16f843315ce5f
gustavoHps126 <gustavo.pf.126@gmail.com>
2025-11-30 13:35:55 -0300

    Finalização curso Pipeline de Dados

--- scripts/fusao_mercado_nov.py
+++ scripts/fusao_mercado_nov.py
@@ -1,58 +1,20 @@
-import json
-import csv
 
-def leitura_json(path_json):
-    dados_json = []
-    with open(path_json, 'r') as file:
-      dados_json = json.load(file)
-    return dados_json
-
-def leitura_csv(path_csv):
-    dados_csv = []
-    with open(path_csv, 'r') as file:
-        spamreader = csv.DictReader(file, delimiter=',')
-        for row in spamreader:
-            dados_csv.append(row)
-    return dados_csv
-
-def letirura_dados(path, tipo_arquvivo):
-    dados = []
-
-    if tipo_arquvivo == 'csv':
-        dados = leitura_csv(path)  
-
-    elif tipo_arquvivo == 'json':
-        dados = leitura_json(path)
-
-    return dados
-
-def rename_columns(dados, key_mapping):
-    new_dados_csv = []
-
-    for old_dict in  dados:
-        dict_temp = {}
-        for old_key, value in old_dict.items():
-            dict_temp[key_mapping[old_key]] = value
-        new_dados_csv.append(dict_temp)
-    new_dados_csv[0]
-
-    return new_dados_csv
-
-def get_columns(dados):
-   return list(dados[0].keys())
+from processamento_dados import Dados
 
 path_csv = 'data_raw/dados_empresaB.csv'
 path_json = 'data_raw/dados_empresaA.json'
 
+#Extract
 
-#Iniciando a Leitura
-dados_json = letirura_dados(path_json, 'json')
-nome_colunas_json = get_columns(dados_json)
-print(f"Nome colunas dados json: {nome_colunas_json}")
+dados_empresaA = Dados(path_json, 'json')
+print(dados_empresaA.nome_colunas)
+print(dados_empresaA.qtd_linhas)
 
-dados_csv = letirura_dados(path_csv, 'csv')
-nome_colunas_csv = get_columns(dados_csv)
-print(nome_colunas_csv)
+dados_empresaB = Dados(path_csv, 'csv')
+print(dados_empresaB.nome_colunas)
+print(dados_empresaB.qtd_linhas)
+
+#Transform
 
 key_mapping = {'Nome do Item': 'Nome do Produto',
              'Classificação do Produto': 'Categoria do Produto',
@@ -61,8 +23,15 @@ key_mapping = {'Nome do Item': 'Nome do Produto',
              'Nome da Loja': 'Filial',
              'Data da Venda': 'Data da Venda'}
 
-#Transformação de dados
+dados_empresaB.rename_columns(key_mapping)
+print(dados_empresaB.nome_colunas)
 
-dados_csv = rename_columns(dados_csv, key_mapping)
-nome_colunas_csv = get_columns(dados_csv)
-print(nome_colunas_csv)
+dados_fusao = Dados.join(dados_empresaA, dados_empresaB)
+print(dados_fusao.nome_colunas)
+print(dados_fusao.qtd_linhas)
+
+#load
+
+path_dados_combinados = 'data_processed/dados_combinados.csv'
+dados_fusao.salvando_dados(path_dados_combinados)
+print(path_dados_combinados)
